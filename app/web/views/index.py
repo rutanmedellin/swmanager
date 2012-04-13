9cd5110ca8b5e1c9b3fbf2a94ce611ca8a56c9c7
juan pablo gaviria <juanpgaviria@gmail.com>
2012-04-13 02:20:19 -0500

    render the web/inde.html templete with the settions mock variable

--- app/web/views/index.py
+++ app/web/views/index.py
@@ -1,5 +1,6 @@
 # view to render the index.html 
 from django.shortcuts import render_to_response
+from django.conf import settings
 import logging
 
 
@@ -7,7 +8,8 @@ def home(request):
     """ Render templates/web/index.html."""
     
     return render_to_response(
-        'web/index.html' 
+        'web/index.html',
+        {'mocks': settings.MOCK_API_FOR_JS }  
         )
 
 
