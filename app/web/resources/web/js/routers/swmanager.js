05dced315f475d1911e34c63c95db0c319e8b964
juan pablo gaviria <juanpgaviria@gmail.com>
2012-04-20 00:36:39 -0500

    addition of the admin routes

--- app/web/resources/web/js/routers/swmanager.js
+++ app/web/resources/web/js/routers/swmanager.js
@@ -10,6 +10,21 @@ App.Routers.StartupWeekendManager = Backbone.Router.extend({
 	
 	routes: {
 		'': "index",
+		'!/admin': "admin", 
+		'!/admin/admin-users': "adminUsers",
+	},
+	
+	/*
+	 * Set nav active item, this has to be call when #main-menu item click
+	 * the item must have a class name exactly the same as the section param
+	 */
+	
+	navActive: function (section){
+		_.each($("#main-menu li"), function (e){$(e).removeClass("active")});
+		if (section != undefined){
+			$("." + section).addClass("active");	
+		}
+		
 	},
 	
 	/*
@@ -17,6 +32,21 @@ App.Routers.StartupWeekendManager = Backbone.Router.extend({
 	 */
 	index: function (){
 		$("#wrapper").html(JST.index());
+		this.navActive("home");
 	},
+	
+	/*
+	 * admin route
+	 */
+	
+	admin: function (section){
+		Data.Views.admin = new App.Views.Admin({el: "#wrapper", model: Data.Models.account, section: section});
+		this.navActive("admin");	
+	},
+	
+	adminUsers: function (){
+		this.admin("admin-users");
+		Data.Views.admin = new App.Views.AdminUsers({el: ".admin-content"});
+	}
 	
 });
