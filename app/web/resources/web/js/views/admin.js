0a26b85ead913aaa0bfdf229a0b371c969c0d44c
juan pablo gaviria <juanpgaviria@gmail.com>
2012-04-20 03:12:57 -0500

    addition of the App.Views.Invitations and App.Views.Invitation views to render invitation that are pending

--- app/web/resources/web/js/views/admin.js
+++ app/web/resources/web/js/views/admin.js
@@ -19,6 +19,71 @@ App.Views.Admin = Backbone.View.extend({
 	}
 });
 
+// invitation view
+App.Views.Invitation = Backbone.View.extend({
+	tagName: "tr",
+	className: "invitation",
+	
+	initialize: function (){
+		_.bindAll(this, 'render', 'avatar', 'resend', 'cancel');
+		this.render();	
+	},
+	
+	events: {
+		"click .resend":	"resend",
+		"click .cancel":	"cancel",
+	},
+	
+	render: function(){
+		this.avatar()				
+		$(this.el).html(JST.user_invitation({model: this.model}));
+		return this;
+	},
+	
+	// get the gravatar url
+	avatar: function (){
+		gravatar_url = Gravatar(this.model.escape("email"));
+		this.model.set({avatar: gravatar_url},{silent: true});
+	},
+	
+	resend: function (){
+		
+	},
+	
+	cancel: function (){
+		
+	}
+}); 
+
+
+// invitations view
+App.Views.Invitations = Backbone.View.extend({
+	tagName: "div",
+	className: "invitations-pending",
+	
+	initialize: function (){
+		_.bindAll(this, 'render', 'addOne', 'addAll');
+		this.render();	
+	},
+	
+	render: function(){
+		admin_invitations = this.collection.where({type: this.options.type});	
+		this.collection = new App.Collections.Invitations(admin_invitations);
+		$(this.el).html(JST.invitations());					
+		this.addAll();
+	},
+	
+	addOne: function (invitation){
+		var view = new App.Views.Invitation({model: invitation});
+		$('table', this.el).append(view.render().el);
+	},
+	
+	addAll: function(){
+		this.collection.each(this.addOne);
+	}
+}); 
+
+// admin users view
 App.Views.AdminUsers = Backbone.View.extend({
 
 	initialize: function (){
@@ -33,6 +98,17 @@ App.Views.AdminUsers = Backbone.View.extend({
 	render: function(){				
 		$(this.el).html(JST.admin_users());
 		$(".invitation-form", this.el).html(JST.invite_user_form({role: "admin"}));
+		Data.Collections.invitations = new App.Collections.Invitations();
+		Data.Collections.invitations.fetch(
+			{
+				success: function (collection, response){
+					new App.Views.Invitations({el: ".invitations-pending", collection: collection, type: "admin"});
+				},
+				error: function (collection, response){
+					
+				}
+			}
+		);
 	},
 	
 	invite: function (e){
