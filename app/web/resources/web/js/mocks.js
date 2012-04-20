8f4a0cbce6b379d9f4834a2c11ff5c8d47ce2bdc
juan pablo gaviria <juanpgaviria@gmail.com>
2012-04-20 05:11:17 -0500

    mockin PUT and DELETE request for /api/v1/invitations/xxx

--- app/web/resources/web/js/mocks.js
+++ app/web/resources/web/js/mocks.js
@@ -10,100 +10,125 @@ $.ajax = function ajaxMock(params) {
 		}]
 	};
 	
-	if(params.type == 'POST' || params.type == 'post'){
+	if (params.type == 'POST' || params.type == 'post') {
 		if (params.url.match('.*sessions$') != null) {
 			var d = $.parseJSON(params.data);
 			
-			data = {'id': 1,
-					'key': "12345",
-					'resource_uri': "/api/v1/sessions/1",
-					'user': {
-						'id': 1,
-						'username': d.username,
-						'first_name': "juan",
-						'last_name': "gaviria",
-						'email': "juanpgaviria@gmail.com",
-						'resource_uri': "/api/v1/users/1"
-						}
-					};
+			data = {
+				'id': 1,
+				'key': "12345",
+				'resource_uri': "/api/v1/sessions/1",
+				'user': {
+					'id': 1,
+					'username': d.username,
+					'first_name': "juan",
+					'last_name': "gaviria",
+					'email': "juanpgaviria@gmail.com",
+					'resource_uri': "/api/v1/users/1"
+				}
+			};
 			response = data;
 			account_data = {
 				user: data
 			};
 		}
-		else if (params.url.match('.*users/[0-9]+$') != null) {
-	  		data = {'id': 1,
+		else 
+			if (params.url.match('.*users/[0-9]+$') != null) {
+				data = {
+					'id': 1,
 					'resource_uri': "/api/v1/users/1",
 					'username': "juanpgaviria",
 					'first_name': "juan",
 					'last_name': "gaviria",
 					'email': "juanpgaviria@gmail.com",
+				};
+			}
+			else 
+				if (params.url.match('.*invitations$') != null) {
+					data = {
+						id: 1,
+						email: "juanpgaviria@gmail.com",
+						role: "admin",
 					};
-	  	}
-		else if (params.url.match('.*invitations$') != null){
-			data = {
-				id: 1,
-				email: "juanpgaviria@gmail.com",
-				role: "admin",
-			};
-		}
+				}
 	}
-	else if (params.type == 'GET' || params.type == 'get'){
-		if (params.url.match('.*users/[0-9]+$') != null) {
-			data = {
-				'id': 1,
-				'resource_uri': "/api/v1/users/1",
-				'username': "juanpgaviria",
-				'first_name': "juan",
-				'last_name': "gaviria",
-				'email': "juanpgaviria@gmail.com",
-				'role': "admin",
-				'bio': "hola mundo",
-				'ideas': "/api/v1/ideas/?user=1",
-				'projects': "/api/v1/projects/?user=1"
-			};
-			response = data;
-		}
-		else if (params.url.match('.*invitations$') != null){
-			data = {
-				"meta": {
-					"limit": 20,
-					 "next": null, 
-					 "offset": 0, 
-					 "previous": null, 
-					 "total_count": 4
-				}, 
-				"objects": [
-					 	{
+	else 
+		if (params.type == 'GET' || params.type == 'get') {
+			if (params.url.match('.*users/[0-9]+$') != null) {
+				data = {
+					'id': 1,
+					'resource_uri': "/api/v1/users/1",
+					'username': "juanpgaviria",
+					'first_name': "juan",
+					'last_name': "gaviria",
+					'email': "juanpgaviria@gmail.com",
+					'role': "admin",
+					'bio': "hola mundo",
+					'ideas': "/api/v1/ideas/?user=1",
+					'projects': "/api/v1/projects/?user=1"
+				};
+				response = data;
+			}
+			else 
+				if (params.url.match('.*invitations$') != null) {
+					data = {
+						"meta": {
+							"limit": 20,
+							"next": null,
+							"offset": 0,
+							"previous": null,
+							"total_count": 4
+						},
+						"objects": [{
 							"id": "4f84b22ade94e65caf000010",
-							"email": "juanpgaviria@gmail.com", 
-							"type": "admin", 
-							"resource_uri": "/api/v1/users/4f84b22ade94e65caf000010/", 
-						},
-						{
+							"email": "juanpgaviria@gmail.com",
+							"role": "admin",
+							"resource_uri": "/api/v1/users/4f84b22ade94e65caf000010/",
+						}, {
 							"id": "4f84b22ade94e65caf000011",
-							"email": "castillobuiles@gmail.com", 
-							"type": "admin", 
-							"resource_uri": "/api/v1/users/4f84b22ade94e65caf000011/", 
-						},
-						{
+							"email": "castillobuiles@gmail.com",
+							"role": "admin",
+							"resource_uri": "/api/v1/users/4f84b22ade94e65caf000011/",
+						}, {
 							"id": "4f84b22ade94e65caf000012",
-							"email": "manuelzs@gmail.com", 
-							"type": "admin", 
-							"resource_uri": "/api/v1/users/4f84b22ade94e65caf000011/", 
-						},
-						{
+							"email": "manuelzs@gmail.com",
+							"role": "admin",
+							"resource_uri": "/api/v1/users/4f84b22ade94e65caf000011/",
+						}, {
 							"id": "4f84b22ade94e65caf000013",
-							"email": "elizabeth.ramirez@rutanmedellin.org", 
-							"type": "participant", 
-							"resource_uri": "/api/v1/users/4f84b22ade94e65caf000011/", 
-						}
-						
-				]
-			};
-			response = data;
+							"email": "elizabeth.ramirez@rutanmedellin.org",
+							"role": "participant",
+							"resource_uri": "/api/v1/users/4f84b22ade94e65caf000011/",
+						}]
+					};
+					response = data;
+				}
 		}
-	}
+		else if (params.type == 'PUT' || params.type == 'put') {
+			log("put");
+				if (params.url.match('.*invitations/[A-Za-z0-9]+$') != null) {
+					log("invitation");
+					var d = $.parseJSON(params.data);
+					data = {
+						"id": d.id,
+						"email": d.email,
+						"role": d.role,
+						"resource_uri": d.resource_uri,
+					};
+					response = data;
+				}
+		}
+		else if (params.type == 'DELETE' || params.type == 'delete') {
+			log("put");
+			if (params.url.match('.*invitations/[A-Za-z0-9]+$') != null) {
+				data = {
+				};
+				response = data;
+			}
+			
+		}else {
+			ajax(params);
+		}
 	params.success(data);
 };
   
