fe8bddc6edafe24edc0aa0d5a4adb94f2a62a273
juan pablo gaviria <juanpgaviria@gmail.com>
2012-04-20 00:40:35 -0500

    addition of the spect test for the admin routes

--- app/web/resources/web/js/spec/routers_spec.js
+++ app/web/resources/web/js/spec/routers_spec.js
@@ -18,4 +18,12 @@ describe("AppRouter routes", function() {
     this.router.navigate("", true);
     expect(this.routeSpy.calledOnce).toBeTruthy();
   });  
+  
+  it("fires the admin route with a #!/admin hash", function (){
+  	this.router.bind("route:admin", this.routeSpy);
+    this.router.navigate("/#!/admin", true);
+	log(location.href);
+    expect(this.routeSpy.calledOnce).toBeTruthy();
+  });
+  
 });
