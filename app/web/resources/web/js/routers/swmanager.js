0a3c9ef2dcbc5b94664f68e0c8de5d0fb970b0c4
juan pablo gaviria <juanpgaviria@gmail.com>
2012-04-24 02:05:55 -0500

    addition of the /user/registration?:params rountes and tests

--- app/web/resources/web/js/routers/swmanager.js
+++ app/web/resources/web/js/routers/swmanager.js
@@ -12,6 +12,7 @@ App.Routers.StartupWeekendManager = Backbone.Router.extend({
 		'': "index",
 		'!/admin': "admin", 
 		'!/admin/admin-users': "adminUsers",
+		'!/user/registration?:params': "registration",
 	},
 	
 	/*
@@ -47,6 +48,46 @@ App.Routers.StartupWeekendManager = Backbone.Router.extend({
 	adminUsers: function (){
 		this.admin("admin-users");
 		Data.Views.admin = new App.Views.AdminUsers({el: ".admin-content"});
-	}
+	},
+	
+	/*
+	 * Registration route
+	 */
+	
+	registration: function (params){
+		var code = (params.code != undefined ? params.code : "");
+		var email = (params.email != undefined ? params.email : "");
+		new App.Views.UserRegistration({el: "#wrapper", code: code, email: email});
+		
+	},
+	
+	_extractParameters: function(route, fragment) {
+        var result = route.exec(fragment).slice(1);
+        result.unshift(deparam(result[result.length-1]));
+        return result.slice(0,-1);
+    }
 	
 });
+
+/*
+ * To be able to get the ?key=value after #..
+ * see http://stackoverflow.com/questions/7445353/key-value-pair-params-handling-in-backbone-js-router and
+ * http://jsfiddle.net/avrelian/h5wL2/ 
+ * 
+ * in th jsfiddle open firebug to see the result :P
+ */
+
+var deparam = function(paramString){
+    var result = {};
+    if( ! paramString){
+        return result;
+    }
+    $.each(paramString.split('&'), function(index, value){
+        if(value){
+            var param = value.split('=');
+            result[param[0]] = param[1];
+        }
+    });
+    return result;
+};
+
