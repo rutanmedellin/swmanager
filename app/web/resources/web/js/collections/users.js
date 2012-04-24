cd596f9dca913b1856d9fa7efdfde6141e08f296
juan pablo gaviria <juanpgaviria@gmail.com>
2012-04-23 19:30:48 -0500

    user model and collection

---
+++ app/web/resources/web/js/collections/users.js
@@ -0,0 +1,18 @@
+App.Collections.Users = App.Collections.BaseCollection.extend({
+	model: App.Models.User,
+	url: "/api/v1/users",
+	parse: function (response){
+		this.limit = response.meta.limit;
+		this.next = response.meta.next;
+		this.offset = response.meta.offset;
+		this.previous = response.meta.previous;
+		this.total_count = response.meta.total_count;
+		
+		return response.objects;
+	},
+	limit: 0,
+	next: null, 
+	offset: 0, 
+	previous: null, 
+	total_count: 0	
+});
