5fb221a35e4fe5f38988a90f5b0c34a5486f8e3c
juan pablo gaviria <juanpgaviria@gmail.com>
2012-04-24 02:06:30 -0500

    adding test to creation methos of the App.Models.User model

--- app/web/resources/web/js/spec/user_model_spec.js
+++ app/web/resources/web/js/spec/user_model_spec.js
@@ -88,6 +88,66 @@ describe("Account Model", function (){
 		});		
 	});
 	
+	describe("When creating a new user", function (){
+		beforeEach(function(){
+			this.account = new App.Models.User({code: "1234", email: "juanpgaviria@gmail.com"});
+			this.callback = sinon.spy();
+			
+			//this.server = sinon.fakeServer.create();
+			
+			// set server response
+			this.user_data = {
+				"id":1, 
+				"username":"admin", 
+				"role": "participant", 
+				"first_name": "juan", 
+				"last_name": "gaviria", 
+				"email": "juanpgaviria@gmail.com", 
+				"ideas": "/api/v1/ideas/?user=1", 
+				"projects": "/api/v1/projects/?user=1"
+			};
+			
+			this.server.respondWith(
+				"POST", 
+				"/api/v1/users",
+				[
+					200, 
+					{"Content-Type": "application/json"},
+					JSON.stringify(this.user_data)
+				]
+			);
+		});
+	
+		afterEach(function() {
+	    	this.server.restore();
+	  	});	
+		
+		it("should the correct request", function (){
+			this.account.save();
+			expect(this.server.requests.length)
+				.toEqual(1);
+			expect(this.server.requests[0].method)
+				.toEqual("POST");
+			expect(this.server.requests[0].url)
+				.toEqual("/api/v1/users");
+		});
+		
+		it("Should return like fetch", function (){
+			this.account.save();
+			this.server.respond();
+			
+			expect(this.account.get("id")).toEqual(1);
+			expect(this.account.get("username")).toEqual("admin");
+			expect(this.account.get("role")).toEqual("participant");
+			expect(this.account.get("first_name")).toEqual("juan");
+			expect(this.account.get("last_name")).toEqual("gaviria");
+			expect(this.account.get("email")).toEqual("juanpgaviria@gmail.com");
+			expect(this.account.get("ideas")).toEqual("/api/v1/ideas/?user=1");
+			expect(this.account.get("projects")).toEqual("/api/v1/projects/?user=1");	
+		});
+		
+	});
+	
 });
 
 
