dbbdabbbd790208d5c9dd6055707c05b73d13d4c
juan pablo gaviria <juanpgaviria@gmail.com>
2012-04-29 20:21:08 -0500

    creation of the ideas collections and test

---
+++ app/web/resources/web/js/collections/ideas.js
@@ -0,0 +1,18 @@
+App.Collections.Ideas = App.Collections.BaseCollection.extend({
+	model: App.Models.Idea,
+	url: "/api/v1/ideas",
+	parse: function (response){
+		this.limit = response.meta.limit;
+		this.next = response.meta.next;
+		this.offset = response.meta.offset;
+		this.previous = response.meta.previous;
+		this.total_count = response.meta.total_count;
+		
+		return response.objects;
+	},
+	limit: 0,
+	next: null, 
+	offset: 0, 
+	previous: null, 
+	total_count: 0	
+});
