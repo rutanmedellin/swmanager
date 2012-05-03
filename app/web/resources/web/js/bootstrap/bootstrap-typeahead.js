e79cc02468e06b69b68c284a62a5d43d051eec77
juan pablo gaviria <juanpgaviria@gmail.com>
2012-05-03 14:57:36 -0500

    addition of remote source support and the posibility to everwrite the select, source, render, matcher, sorter methods

--- app/web/resources/web/js/bootstrap/bootstrap-typeahead.js
+++ app/web/resources/web/js/bootstrap/bootstrap-typeahead.js
@@ -1,5 +1,5 @@
 /* =============================================================
- * bootstrap-typeahead.js v2.0.2
+ * bootstrap-typeahead.js v2.0.0
  * http://twitter.github.com/bootstrap/javascript.html#typeahead
  * =============================================================
  * Copyright 2012 Twitter, Inc.
@@ -26,9 +26,14 @@
     this.options = $.extend({}, $.fn.typeahead.defaults, options)
     this.matcher = this.options.matcher || this.matcher
     this.sorter = this.options.sorter || this.sorter
+	this.render = this.options.render || this.render
+	this.process = this.options.process || this.process
+	this.select = this.options.select || this.select
     this.highlighter = this.options.highlighter || this.highlighter
     this.$menu = $(this.options.menu).appendTo('body')
     this.source = this.options.source
+    this.onselect = this.options.onselect
+    this.strings = true
     this.shown = false
     this.listen()
   }
@@ -38,9 +43,17 @@
     constructor: Typeahead
 
   , select: function () {
-      var val = this.$menu.find('.active').attr('data-value')
-      this.$element.val(val)
-      this.$element.change();
+      var val = JSON.parse(this.$menu.find('.active').attr('data-value'))
+        , text
+
+      if (!this.strings) text = val[this.options.property]
+      else text = val
+
+      this.$element.val(text)
+
+      if (typeof this.onselect == "function")
+          this.onselect(val)
+
       return this.hide()
     }
 
@@ -69,6 +82,25 @@
       var that = this
         , items
         , q
+        , value
+
+      this.query = this.$element.val()
+
+      if (typeof this.source == "function") {
+        value = this.source(this, this.query)
+        if (value) this.process(value)
+      } else {
+        this.process(this.source)
+      }
+    }
+
+  , process: function (results) {
+      var that = this
+        , items
+        , q
+
+      if (results.length && typeof results[0] != "string")
+          this.strings = false
 
       this.query = this.$element.val()
 
@@ -76,7 +108,9 @@
         return this.shown ? this.hide() : this
       }
 
-      items = $.grep(this.source, function (item) {
+      items = $.grep(results, function (item) {
+        if (!that.strings)
+          item = item[that.options.property]
         if (that.matcher(item)) return item
       })
 
@@ -98,10 +132,14 @@
         , caseSensitive = []
         , caseInsensitive = []
         , item
+        , sortby
 
       while (item = items.shift()) {
-        if (!item.toLowerCase().indexOf(this.query.toLowerCase())) beginswith.push(item)
-        else if (~item.indexOf(this.query)) caseSensitive.push(item)
+        if (this.strings) sortby = item
+        else sortby = item[this.options.property]
+
+        if (!sortby.toLowerCase().indexOf(this.query.toLowerCase())) beginswith.push(item)
+        else if (~sortby.indexOf(this.query)) caseSensitive.push(item)
         else caseInsensitive.push(item)
       }
 
@@ -118,7 +156,9 @@
       var that = this
 
       items = $(items).map(function (i, item) {
-        i = $(that.options.item).attr('data-value', item)
+        i = $(that.options.item).attr('data-value', JSON.stringify(item))
+        if (!that.strings)
+            item = item[that.options.property]
         i.find('a').html(that.highlighter(item))
         return i[0]
       })
@@ -166,6 +206,9 @@
     }
 
   , keyup: function (e) {
+      e.stopPropagation()
+      e.preventDefault()
+
       switch(e.keyCode) {
         case 40: // down arrow
         case 38: // up arrow
@@ -178,7 +221,6 @@
           break
 
         case 27: // escape
-          if (!this.shown) return
           this.hide()
           break
 
@@ -186,11 +228,10 @@
           this.lookup()
       }
 
-      e.stopPropagation()
-      e.preventDefault()
   }
 
   , keypress: function (e) {
+      e.stopPropagation()
       if (!this.shown) return
 
       switch(e.keyCode) {
@@ -210,12 +251,12 @@
           this.next()
           break
       }
-
-      e.stopPropagation()
     }
 
   , blur: function (e) {
       var that = this
+      e.stopPropagation()
+      e.preventDefault()
       setTimeout(function () { that.hide() }, 150)
     }
 
@@ -251,6 +292,8 @@
   , items: 8
   , menu: '<ul class="typeahead dropdown-menu"></ul>'
   , item: '<li><a href="#"></a></li>'
+  , onselect: null
+  , property: 'value'
   }
 
   $.fn.typeahead.Constructor = Typeahead
