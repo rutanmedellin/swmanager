fab01ee01a547f45dfefa1b4dfc149fd6a38ea8e
scastillo <castillobuiles@gmail.com>
2012-05-22 13:09:44 -0500

    Added event resource

---
+++ app/api/resources/event_resource.py
@@ -0,0 +1,14 @@
+from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
+from tastypie import fields
+
+from core.models import Event
+
+
+class EventResource(ModelResource):
+    class Meta:
+        resource_name = 'events'
+        queryset = Event.objects.all()
+        authorization = Authorization()
+        always_return_data = True
+        allowed_methods = ['post', 'get']
