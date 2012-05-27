b99ba0b268045c9c56de9b34b4975c4731efe5e8
scastillo <castillobuiles@gmail.com>
2012-05-27 15:13:51 -0500

    Added votes to ideas

--- app/api/resources/idea_resource.py
+++ app/api/resources/idea_resource.py
@@ -4,14 +4,14 @@ from tastypie.authorization import Authorization
 from tastypie.authentication import ApiKeyAuthentication
 from tastypie import fields
 
-from core.models import Idea
+from core.models import Idea, Vote
 from api.resources import UserResource
 
 
 class IdeaResource(ModelResource):
     participant = fields.ToOneField('api.resources.UserResource', 'participant', full=True)
     #participant = fields.ToOneField(UserResource, 'participant', full=True)
-    
+
     class Meta:
         queryset = Idea.objects.all()
         resource_name = 'ideas'
@@ -19,3 +19,9 @@ class IdeaResource(ModelResource):
         #authentication = ApiKeyAuthentication()
         always_return_data = True
         allowed_methods = ['post', 'get']
+
+    def dehydrate(self, bundle):
+        bundle.data['votes'] = Vote.objects.filter(vote_type='idea',
+                                                    type_id=bundle.obj.id).count()
+                                                    
+        return bundle
