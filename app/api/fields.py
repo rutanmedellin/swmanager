1aff3e2cd1bbc73331251893932d13c9d1e12ffb
scastillo <castillobuiles@gmail.com>
2012-05-29 02:59:01 -0500

    Deprecated ListField

--- app/api/fields.py
+++ app/api/fields.py
@@ -1,19 +1,21 @@
-from tastypie.fields import ApiField
+# deprecated with relational branch merge
 
-
+#from tastypie.fields import ApiField
+#
+#
 # From https://github.com/andresdouglas/django-tastypie-nonrel/blob/master/tastypie_nonrel/fields.py
-class ListField(ApiField):
-    """
-        Represents a list of simple items - strings, ints, bools, etc. For
-        embedding objects use EmbeddedListField in combination with EmbeddedModelField
-        instead.
-    """
-    dehydrated_type     =   'list'
-
-    def dehydrate(self, obj):
-        return self.convert(super(ListField, self).dehydrate(obj))
-
-    def convert(self, value):
-        if value is None:
-            return None
-        return value 
+#class ListField(ApiField):
+#    """
+#        Represents a list of simple items - strings, ints, bools, etc. For
+#        embedding objects use EmbeddedListField in combination with EmbeddedModelField
+#        instead.
+#    """
+#    dehydrated_type = 'list'
+#    
+#    def dehydrate(self, obj):
+#        return self.convert(super(ListField, self).dehydrate(obj))
+#
+#    def convert(self, value):
+#        if value is None:
+#            return None
+#        return value 
