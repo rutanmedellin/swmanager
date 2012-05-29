f5798c26d00b0239870dc924902a0ee5401af30a
scastillo <castillobuiles@gmail.com>
2012-05-29 13:42:36 -0500

    Team could be null

--- app/core/models/project.py
+++ app/core/models/project.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     owner = models.ForeignKey(User, related_name='my_project')
     name = models.CharField(max_length=512)
     description = models.TextField()
-    team = models.ManyToManyField(User, related_name='projects')
+    team = models.ManyToManyField(User, related_name='projects', blank=True, null=True)
     
     class Meta:
         app_label = 'core'
