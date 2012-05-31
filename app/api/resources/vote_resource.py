fa9af7e659185407be6973fd5d8ad5d2dfe14ef1
scastillo <castillobuiles@gmail.com>
2012-05-31 18:36:04 -0500

    Added votes to users and limit votes to 3

--- app/api/resources/vote_resource.py
+++ app/api/resources/vote_resource.py
@@ -1,13 +1,30 @@
 from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization
+from tastypie.authentication import ApiKeyAuthentication
+from tastypie.validation import Validation
 
 from core.models import Vote
 
+
+class VoteResourceValidation(Validation):
+    def is_valid(self, bundle, request=None):
+        errors = super(VoteResourceValidation, self).is_valid(bundle, request)
+        print "user: %s" % request.user
+        vote_count = Vote.objects.filter(user_id=request.user.id).count()
+        if vote_count >= 3:
+            errors['count'] = "Oh boy, just 3 votes, don't be greedy ;)"
+        print "count= %s" % vote_count
+        return errors
 
 class VoteResource(ModelResource):
     class Meta:
         queryset = Vote.objects.all()
         resource_name = 'votes'
         authorization = Authorization()
+        authentication = ApiKeyAuthentication()
+        validation = VoteResourceValidation()
+        
         always_return_data = True
         allowed_methods = ['post', 'get']
+        
+        
--- app/core/models/vote.py
+++ app/core/models/vote.py
@@ -1,15 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms.models import model_to_dict
 
 
 # Ugliest model ever just for time reasosns and bad front-end design :(
 # TODO Change me!!: User real relations instead of ids for example.
 class Vote(models.Model):
     "Geniric vote model"
-    user = models.ManyToManyField(User, related_name='votes')
+    user = models.ForeignKey(User, related_name='votes', null=True, blank=True)
     type_id = models.CharField(max_length=255)
     vote_type = models.CharField(max_length=64)
 
+    def __unicode__(self):
+        return str(model_to_dict(self))
+    
     class Meta:
         app_label = 'core'
         verbose_name_plural = 'Vote'
+        
