cdf654549e36261fba31cc12820c73a00dc07e3b
scastillo <castillobuiles@gmail.com>
2012-06-01 16:53:46 -0500

    Added full projects team, url and email

--- app/core/models/project.py
+++ app/core/models/project.py
@@ -7,7 +7,10 @@ class Project(models.Model):
     name = models.CharField(max_length=512)
     description = models.TextField()
     twitter = models.CharField(max_length=128, default="")
+    url = models.URLField(default="")
+    email = models.EmailField(default="")
     team = models.ManyToManyField(User, related_name='projects', blank=True, null=True)
+
     
     class Meta:
         app_label = 'core'
